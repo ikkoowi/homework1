8bde703ddaa18a56112e5bb7a66c05bccd7e4b94
ikkoowigit config --global user.email <ikk6455@gmail.com>
2024-02-09 22:31:45 +0400

    внесла правки

--- hw2.js
+++ hw2.js
@@ -5,11 +5,11 @@ let b = 20;
 alert(b);
 
 // Задание 2
-let launchYear=2007;
+let launchYear = 2007;
 alert(`Год выпуска первого iphone ${launchYear}`);
 
 // Задание 3
-let creator="Брендан Эйх";
+let creator = "Брендан Эйх";
 alert(`Создатель языка JavaScript ${creator}`);
 
 // Задание 4
@@ -23,7 +23,7 @@ alert(c / d);
 // Задание 5
 const e = 2;
 let result = 2 ** 5;
-alert (result);
+alert(result);
 
 // Задание 6
 const f = 9;
@@ -45,20 +45,20 @@ let age = Number(prompt("Сколько вам лет?"));
 alert(age);
 
 //9.0
-let user= {
+let user = {
     name: 'Vasya',
     age: 33,
     isAdmin: true
 };
 
 //9.1
-user["cityOfResidence"] = true;
+user["city of residence"] = true;
 
 //9.2
 user.age = 34;
 
 //9.3
-delete user["cityOfResidence"];
+delete user["city of residence"];
 
 //9.4
 let info = prompt("Какую информацию хотите узнать о пользователе?");
